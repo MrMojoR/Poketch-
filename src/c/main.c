52c96efdf09f0c56452ae6b1763d167daab9ed3a
MrMojoR <osvathmarton@gmail.com>
2017-09-30 16:49:07 +0200

    Added steps and day name

--- src/c/main.c
+++ src/c/main.c
@@ -12,8 +12,8 @@
 // Colors
 #ifdef PBL_COLOR
   #define PALETTE_SIZE    2
-  #define COLOR_FG(day)   ((day) ? GColorDarkGreen : GColorScreaminGreen)
-  #define COLOR_BG(day)   ((day) ? GColorScreaminGreen : GColorDarkGreen)
+  #define COLOR_FG(day)   ((day) ? GColorDarkGreen : GColorMalachite)
+  #define COLOR_BG(day)   ((day) ? GColorMalachite : GColorDarkGreen)
 #else
   #define COLOR_FG(day)   ((day) ? GColorBlack : GColorWhite)
   #define COLOR_BG(day)   ((day) ? GColorWhite : GColorBlack)
@@ -28,6 +28,12 @@
 #define FMT_SEC         "%S"
 #define FMT_SEC_LEN     sizeof("00")
 
+#define FMT_STEPS        "%d"
+#define FMT_STEPS_LEN     sizeof("00000")
+
+#define FMT_DAY        "%s"
+#define FMT_DAY_LEN     sizeof("DAY")
+
 #define TIMEOUT_SECDATE 3000
 
 #if defined(PBL_RECT)
@@ -36,11 +42,17 @@
   #define RECT_TIME       GRect(23, 40, 139, 70)
 #endif
 #define RECT_SECDATE    GRect(76, 124, 68, 30)
+#define RECT_STEPS    	GRect(18, 10, 72,168)
+#define RECT_DAY   		  GRect(60, 10, 70,168-10)
 #define RECT_PIKA       GRect(0, 122, 144, 48)
 #define RECT_BANG       GRect(18, 128, 4, 16)
 #define RECT_BAT        GRect(60, 160, 80, 2)
 #define BOUND_BAT(pct)  GRect(0, 0, (pct / 5) * 4, 2)
 #define RECT_CHG        GRect(26, 140, 48, 16)
+
+//Weekdays
+
+const char* dayNames[7] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 
 // === Layers ===
 
@@ -52,6 +64,12 @@ static GFont s_time_font;
 
 static TextLayer *s_secdate_layer;
 static GFont s_secdate_font;
+
+static TextLayer *s_steps_layer;
+static GFont s_steps_font;
+
+static TextLayer *s_day_layer;
+static GFont s_day_font;
 
 static BitmapLayer *s_bang_layer;
 static GBitmap *s_bang;
@@ -102,16 +120,31 @@ static void update_secdate(struct tm *tick_time) {
 
   static char date_buf[FMT_DATE_LEN];
   static char sec_buf[FMT_SEC_LEN];
+  static char day_buf[FMT_DAY_LEN];
+  
+  int weekday = tick_time->tm_wday;
 
   // Update the buffer
-  if (showDate)
+  if (showDate){
     strftime(date_buf, FMT_DATE_LEN, FMT_DATE, tick_time);
+	snprintf(day_buf, FMT_DAY_LEN, FMT_DAY, dayNames[weekday]);
+  }
   else
     strftime(sec_buf, FMT_SEC_LEN, FMT_SEC, tick_time);
 
   // Set the buffer
+  text_layer_set_text(s_day_layer, day_buf);
   text_layer_set_text(s_secdate_layer, showDate ? date_buf : sec_buf);
   layer_mark_dirty(text_layer_get_layer(s_secdate_layer));
+  layer_mark_dirty(text_layer_get_layer(s_day_layer));
+}
+
+static void update_steps() {
+	static char steps_buffer[FMT_STEPS_LEN];
+	unsigned short int steps_day = health_service_sum_today(HealthMetricStepCount);
+  snprintf(steps_buffer,FMT_STEPS_LEN,FMT_STEPS,steps_day);
+	text_layer_set_text(s_steps_layer, steps_buffer);
+	layer_mark_dirty(text_layer_get_layer(s_steps_layer));
 }
 
 static void show_seconds(void *data) {
@@ -140,6 +173,10 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
   // == Secs/date ==
   update_secdate(tick_time);
+  
+  update_steps();
+  
+  layer_mark_dirty(text_layer_get_layer(s_steps_layer));
 
   // === Day/night color inversion ===
   static bool day = true;
@@ -159,6 +196,8 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
       colorize(s_chg_layer, day);
       text_layer_set_text_color(s_time_layer, COLOR_FG(day));
       text_layer_set_text_color(s_secdate_layer, COLOR_FG(day));
+	    text_layer_set_text_color(s_steps_layer, COLOR_FG(day));
+      text_layer_set_text_color(s_day_layer, COLOR_FG(day));
       window_set_background_color(s_main_window, COLOR_BG(day));
       layer_mark_dirty(s_root_layer);
     }
@@ -182,6 +221,8 @@ static void acc_handler(AccelAxisType axis, int32_t direction) {
   if (!(timer_secdate && app_timer_reschedule(timer_secdate, TIMEOUT_SECDATE)))
     // If it doesn't exist or already elapsed, schedule a new timer
     timer_secdate = app_timer_register(TIMEOUT_SECDATE, show_seconds, NULL);
+	
+  update_steps();
 }
 
 static void bt_handler(bool connected) {
@@ -230,6 +271,22 @@ static void main_window_load(Window *window) {
   text_layer_set_background_color(s_secdate_layer, GColorClear);
   text_layer_set_text_alignment(s_secdate_layer, GTextAlignmentCenter);
   layer_add_child(s_root_layer, text_layer_get_layer(s_secdate_layer));
+  
+  // Steps
+  s_steps_layer = text_layer_create(RECT_STEPS);
+  s_steps_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_POKETCH_DIGITAL_27));
+  text_layer_set_font(s_steps_layer, s_steps_font);
+  text_layer_set_background_color(s_steps_layer, GColorClear);
+  text_layer_set_text_alignment(s_steps_layer, GTextAlignmentLeft);
+  layer_add_child(s_root_layer, text_layer_get_layer(s_steps_layer));
+  
+  // Day
+  s_day_layer = text_layer_create(RECT_DAY);
+  s_day_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SQUARE_27));
+  text_layer_set_font(s_day_layer, s_day_font);
+  text_layer_set_background_color(s_day_layer, GColorClear);
+  text_layer_set_text_alignment(s_day_layer, GTextAlignmentRight);
+  layer_add_child(s_root_layer, text_layer_get_layer(s_day_layer));
 
   // Bluetooth
   s_bang_layer = bitmap_layer_create(RECT_BANG);
@@ -280,6 +337,14 @@ static void main_window_unload(Window *window) {
   // Secs/date
   text_layer_destroy(s_secdate_layer);
   fonts_unload_custom_font(s_secdate_font);
+  
+  // Steps
+  text_layer_destroy(s_steps_layer);
+  fonts_unload_custom_font(s_steps_font);
+  
+   // Steps
+  text_layer_destroy(s_day_layer);
+  fonts_unload_custom_font(s_day_font);
 
   // Bluetooth
   bitmap_layer_destroy(s_bang_layer);
@@ -298,7 +363,7 @@ static void init() {
 
   // Register services
   tick_timer_service_subscribe(TICK_UNIT, tick_handler);
-//   accel_tap_service_subscribe(acc_handler);
+  //accel_tap_service_subscribe(acc_handler);
   bluetooth_connection_service_subscribe(bt_handler);
   battery_state_service_subscribe(bat_handler);
 
@@ -318,7 +383,7 @@ static void deinit() {
 
   // Unregister services
   tick_timer_service_unsubscribe();
-//   accel_tap_service_unsubscribe();
+  //accel_tap_service_unsubscribe();
   bluetooth_connection_service_unsubscribe();
   battery_state_service_unsubscribe();
 }
