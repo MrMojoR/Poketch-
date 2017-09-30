7323bce4c575418756ece9ef93d59211fc134e51
MrMojoR <osvathmarton@gmail.com>
2017-09-30 21:51:42 +0200

    Added weather

---
+++ src/pkjs/weather.js
@@ -0,0 +1,67 @@
+
+
+
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log('AppMessage received!');
+    getWeather();
+  }                     
+);
+
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+
+function getWeather() {
+   // Construct URL
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=46.08&lon=18.23&appid=a7973e71a5d0f3ba5d1af51e374f01bc';
+
+  // Send request to OpenWeatherMap
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      var json = JSON.parse(responseText);
+
+      // Temperature in Kelvin requires adjustment
+      var temperature = Math.round(json.main.temp - 273.15);
+      console.log('Temperature is ' + temperature);
+
+      // Conditions
+      var conditions = json.weather[0].main;      
+      console.log('Conditions are ' + conditions);
+      // Assemble dictionary using our keys
+var dictionary = {
+  'TEMPERATURE': temperature,
+  'CONDITIONS': conditions
+};
+
+// Send to Pebble
+Pebble.sendAppMessage(dictionary,
+  function(e) {
+    console.log('Weather info sent to Pebble successfully!');
+  },
+  function(e) {
+    console.log('Error sending weather info to Pebble!');
+  }
+);
+    }      
+  );
+}
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log('PebbleKit JS ready!');
+
+    // Get the initial weather
+    getWeather();
+  }
+);
